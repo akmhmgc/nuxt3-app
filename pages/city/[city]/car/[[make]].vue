8c5f792b62275927e94f1f6b4f67b0d3425b1923
akmhmgc <akimuhamaguchi@icloud.com>
2022-12-18 21:22:06 +0900

    feat: componentsの追加

--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-  <h2>city and make</h2>
+    <NavBar />
+    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+      <div class="mt-32 flex">
+        <CarSideBar />
+        <CarCards />
+      </div>
+    </div>
   </div>
 </template>
