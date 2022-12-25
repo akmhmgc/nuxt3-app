29b50aa011f6aa0796a23cf20627c885c7a621d9
akmhmgc <akimuhamaguchi@icloud.com>
2022-12-25 12:43:25 +0900

    feat: 車の詳細ページを動的に表示する

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,17 +1,27 @@
 <script setup>
+const { cars } = useCars()
 const route = useRoute()
 useHead({
   title: route.params.name
 })
 
+
 definePageMeta({
   layout: "custom"
 })
 
+const car = computed(() => {
+  return cars.find((car) => {
+    return car.id === parseInt(route.params.id)
+  })
+})
+
 </script>
 <template>
-  <CarDetailHero />
-  <CarDetailAttributes />
-  <CarDetailDescription />
-  <CarDetailContact />
+  <div v-if="car">
+    <CarDetailHero :car="car" />
+    <CarDetailAttributes :features="car.features" />
+    <CarDetailDescription :description="car.description" />
+    <CarDetailContact />
+  </div>
 </template>
