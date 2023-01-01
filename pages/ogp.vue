fe32b042d802cb32f7d0048cafbd6d6157aae2b0
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-01 11:12:16 +0900

    wip opg sample

---
+++ pages/ogp.vue
@@ -0,0 +1,13 @@
+<script setup>
+useHead({
+  title: 'OgpTest',
+  meta: [
+    { property: 'og:image', content: 'https://nuxt.com/social.jpg' },
+  ]
+})
+</script>
+<template>
+  <div>
+    <h2>ogp sampleじゃ</h2>
+  </div>
+</template>
