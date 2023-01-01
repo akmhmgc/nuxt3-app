6f84e05a34169155d0c77bdc2306f094becfa2dd
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-01 15:19:01 +0900

    fixup! wip opg sample

---
+++ pages/ogp/[imageId].vue
@@ -0,0 +1,19 @@
+<script setup>
+import heartFilled from "@/assets/heartFilled.png"
+const route = useRoute();
+
+useHead({
+  title: 'OgpTest',
+  meta: [
+    { property: 'og:image', content: heartFilled },
+  ]
+})
+</script>
+<template>
+  <div>
+    <h2>ogp sampleじゃ</h2>
+    <h2>{{ route.params.imageId }}</h2>
+    <h2>{{ route.fullPath }}</h2>
+    <h2>{{ route.path }}</h2>
+  </div>
+</template>
